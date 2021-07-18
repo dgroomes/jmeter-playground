fa89477fdb400b29f1065fc71df94920058b1934
David Groomes <dwgroomes@gmail.com>
2021-07-18 13:31:33 -0500

    Upgrade to JMeter 5.4.1 (latest)
    Phew that was harder than I thought it would be. There is no BOM/parent support.

--- custom-sampler/build.gradle.kts
+++ custom-sampler/build.gradle.kts
@@ -6,18 +6,27 @@ repositories {
     mavenCentral()
 }
 
-val jmeterVersion = "5.1.1"
-val slf4jVersion = "1.7.30"
+val slf4jVersion = "1.7.30" // Make sure to use the version that is bundled with JMeter!
+val jmeterVersion =
+    "5.4.1" // releases: https://jmeter.apache.org/changes_history.html AND https://github.com/apache/jmeter/releases
 
 dependencies {
     /*
     Specify the JMeter dependencies as `compileOnly`. These dependencies *do not* need to be included in the
-    distribution because they are already included in JMeter itself. So, they are needed only as compile-time
-    dependencies. SLF4J is also included in JMeter.
-     */
-    compileOnly(group = "org.apache.jmeter", name = "ApacheJMeter_java", version = jmeterVersion)
-    compileOnly(platform("org.apache.jmeter:ApacheJMeter_parent:$jmeterVersion"))
-    compileOnly(group = "org.slf4j", name = "slf4j-api", version = slf4jVersion)
+    distribution because they are already bundled in JMeter itself. So, they are needed only as compile-time
+    dependencies. SLF4J is also bundled in JMeter.
+
+    To see the versions of dependencies that are bundled in JMeter, refer to the gradle.properties file in the JMeter project, like this https://github.com/apache/jmeter/blob/rel/v5.4.1/gradle.properties#L122
+    */
+    compileOnly("org.apache.jmeter:ApacheJMeter_java:$jmeterVersion")
+    compileOnly("org.slf4j:slf4j-api:$slf4jVersion")
+}
+
+configurations.all {
+    exclude(
+        group = "org.apache.jmeter",
+        module = "bom"
+    ) // Exclude the JMeter "bom" dependency (Bill of Materials) because it doesn't actually exist. See https://bz.apache.org/bugzilla/show_bug.cgi?id=64465 and https://discuss.gradle.org/t/opt-out-of-gradle-module-metadata-for-a-specific-dependency/37051/2
 }
 
 java {
--- with-dependencies/build.gradle.kts
+++ with-dependencies/build.gradle.kts
@@ -11,31 +11,49 @@ repositories {
     mavenCentral()
 }
 
-val jacksonVersion = "2.11.0"
-val jmeterVersion = "5.1.1"
-val slf4jVersion = "1.7.30"
+val slf4jVersion = "1.7.30" // Make sure to use the version that is bundled with JMeter!
+val jacksonVersion = "2.10.5" // Make sure to use the version that is bundled with JMeter!
+val jmeterVersion =
+    "5.4.1" // releases: https://jmeter.apache.org/changes_history.html AND https://github.com/apache/jmeter/releases
 
 dependencies {
     /*
     Specify the JMeter dependencies as `compileOnly`. These dependencies *do not* need to be included in the
     distribution because they are already included in JMeter itself. So, they are needed only as compile-time
-    dependencies. SLF4J is also included in JMeter.
+    dependencies. Among the dependencies included in JMeter are SLF4J and the core Jackson modules.
      */
     compileOnly(group = "org.apache.jmeter", name = "ApacheJMeter_java", version = jmeterVersion)
-    compileOnly(platform("org.apache.jmeter:ApacheJMeter_parent:$jmeterVersion"))
-    compileOnly(group = "org.slf4j", name = "slf4j-api", version = slf4jVersion)
+    /*
+    Among the dependencies included in JMeter are SLF4J and the core Jackson modules. These dependencies are not visible
+    in the Gradle build unless we explicitly declare a dependency on them. So, we must declare them as dependencies. Again,
+    we only use "compileOnly" and not "implementation" because we only need them to compile against. They are already bundled
+    inside of the JMeter distribution itself. With this knowledge, we can code our JMeter plugin to use the specific version
+    of Jackson bundled in JMeter and avoid a "dependency hell" situation where we might be trying to use a newer version
+    of Jackson which breaks JMeter at runtime.
+
+    To see the versions of dependencies that are bundled in JMeter, refer to the gradle.properties file in the JMeter project, like this https://github.com/apache/jmeter/blob/rel/v5.4.1/gradle.properties#L95
+     */
+    compileOnly("org.slf4j:slf4j-api:$slf4jVersion")
+    compileOnly("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
 
     /*
     Here is a library that is *not* included in JMeter. So, we specify it as a "implementation" dependency and therefore
     it will get included in the distribution.
      */
-    implementation(group = "com.fasterxml.jackson.dataformat", name = "jackson-dataformat-yaml", version = jacksonVersion) {
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion") {
         /*
         Some of the Jackson modules are already included in JMeter, so we should exclude them so they do not get
         included in the distribution. It would be redundant to include them twice.
          */
         exclude(group = "com.fasterxml.jackson.core")
     }
+}
+
+configurations.all {
+    exclude(
+        group = "org.apache.jmeter",
+        module = "bom"
+    ) // Exclude the JMeter "bom" dependency (Bill of Materials) because it doesn't actually exist. See https://bz.apache.org/bugzilla/show_bug.cgi?id=64465 and https://discuss.gradle.org/t/opt-out-of-gradle-module-metadata-for-a-specific-dependency/37051/2
 }
 
 java {
