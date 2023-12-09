87538f35fd4401d6f00fcdedb7b52e8a935fa56a
David Groomes <dwgroomes@gmail.com>
2023-12-09 14:10:43 -0600

    Upgrade to Gradle 8.5 and clean up README

--- README.md
+++ README.md
@@ -2,34 +2,39 @@
 
 📚 Learning and exploring Apache JMeter <https://jmeter.apache.org>.
 
-## Standalone sub-projects
 
-This repository illustrates different concepts, patterns and examples via standalone sub-projects. Each sub-project is
-completely independent of the others and do not depend on the root project. This _standalone sub-project constraint_
-forces the sub-projects to be complete and maximizes the reader's chances of successfully running, understanding, and
+## Standalone subprojects
+
+This repository illustrates different concepts, patterns and examples via standalone subprojects. Each subproject is
+completely independent of the others and do not depend on the root project. This _standalone subproject constraint_
+forces the subprojects to be complete and maximizes the reader's chances of successfully running, understanding, and
 re-using the code.
 
-The sub-projects include:
+The subprojects include:
+
 
 ### `custom-sampler/`
 
-This beginner-friendly sub-project creates a custom JMeter "Sampler" to exercise some custom Java code.
+This beginner-friendly subproject creates a custom JMeter "Sampler" to exercise some custom Java code.
 
 See the README in [custom-sampler/](custom-sampler/).
 
+
 ### `with-dependencies/`
 
-This sub-project creates a custom JMeter "Sampler" using custom Java code and additional Java library dependencies.
+This subproject creates a custom JMeter "Sampler" using custom Java code and additional Java library dependencies.
 
 See the README in [with-dependencies/](with-dependencies/).
+
 
 ## Wish List
 
 General clean-ups, TODOs and things I wish to implement for this project:
 
-* DONE Split the Jackson library example out of `custom-sampler/` into its own sub-project `with-dependencies/`
+* [x] DONE Split the Jackson library example out of `custom-sampler/` into its own subproject `with-dependencies/`
 
-## Referenced Materials
+
+## Reference
 
 * [Apache JMeter: Apache project page](https://jmeter.apache.org)
 * [Apache JMeter: GitHub repo](https://github.com/apache/jmeter)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "jmeter-playground"
 
-// What is the point of defining a Gradle project in the root of this repository? After all, each of the sub-projects
+// What is the point of defining a Gradle project in the root of this repository? After all, each of the subprojects
 // are independently runnable... What does it mean to "run the root project"?
 //
 // Answer:
@@ -13,14 +13,14 @@ rootProject.name = "jmeter-playground"
 //
 // Unfortunately, Intellij does not automatically recognize "orphaned" projects. In other words, if you opened this
 // project in Intellij and there was no 'settings.gradle.kts' file in the root directory then Intellij would just give
-// up. It would not know to go into each of the sub-project directories and recognize them as standalone Gradle
+// up. It would not know to go into each of the subproject directories and recognize them as standalone Gradle
 // projects. You would not get the out-of-the-box code completion and project navigation that you are used to with
-// Intellij. Instead, you would have to go to the Intellij "Project Structure" settings and add the sub-projects
+// Intellij. Instead, you would have to go to the Intellij "Project Structure" settings and add the subprojects
 // one-by-one as "Intellij Modules". This is a poor user experience.
 //
-// To avoid this poor user experience, we can define a root Gradle project and include each of the sub-projects via
+// To avoid this poor user experience, we can define a root Gradle project and include each of the subprojects via
 // Gradle "included builds". See https://docs.gradle.org/current/userguide/composite_builds.html. Now, when someone
-// clones this repo and opens it in Intellij, all the sub-projects are automatically identified by Intellij and indexed.
+// clones this repo and opens it in Intellij, all the subprojects are automatically identified by Intellij and indexed.
 // You get code completion and project navigation out-of-the-box. Pretty slick!
 includeBuild("custom-sampler")
 includeBuild("with-dependencies")
